5fc4b08ee04760e8c5299c37e6589b7dbdee2945
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-07-08 15:35:36 +0700

    add message util

---
+++ FtpSharp.Server/MessageUtil.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+
+namespace FtpSharp.Server
+{
+    public static class MessageUtil
+    {
+        public static readonly byte[] CarriageReturn = new byte[] {13, 10};
+        public static readonly byte[] NewLine = new byte[] {10};
+
+        public static byte[] EOL(DataType dataType)
+        {
+            switch (dataType)
+            {
+                case DataType.ASCII:
+                    return CarriageReturn;
+                case DataType.BINARY:
+                    return NewLine;
+                default:
+                    return NewLine;
+            }
+        }
+        public static byte[] SendReply(ClientObject clientObject, int code)
+        {
+            string reply = Reply.GetReply(code);
+            byte[] byteData = Encoding.ASCII.GetBytes(reply);
+            byteData = byteData.Concatenate(EOL(clientObject.dataType));
+            return byteData;
+        }
+    }
+}

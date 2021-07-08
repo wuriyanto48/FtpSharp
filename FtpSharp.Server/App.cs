f111e8769d46c8cf39552aa8ee7e0e3fdd725439
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-07-08 13:09:50 +0700

    rename entry to App.cs

---
+++ FtpSharp.Server/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace FtpSharp.Server
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ECommand.ABOR.ToString());
+            using var server = new Server("localhost", 8777);
+            server.Bind();
+            server.Start();
+        }
+    }
+}

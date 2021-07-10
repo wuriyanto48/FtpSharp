3c10ebaaf000b3dad7a3a9dd5e3741ff1b5d437e
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-07-10 13:49:03 +0700

    fix TYPE command

--- FtpSharp.Server/Src/Command/TYPECommand.cs
+++ FtpSharp.Server/Src/Command/TYPECommand.cs
@@ -20,18 +20,21 @@ namespace FtpSharp.Server.Command
             if (dataType == "A")
             {
                 _clientObject.DataType = DataType.ASCII;
-                byte[] data = MessageUtil.BuildReply(_clientObject, 200, "TYPE set to ASCII");
-                _clientObject.Write(data);
-            } else if (dataType == "I")
+                byte[] daresponseSetAsciidatata = MessageUtil.BuildReply(_clientObject, 200, "TYPE set to ASCII");
+                _clientObject.Write(daresponseSetAsciidatata);
+                return;
+            }  
+            
+            if (dataType == "I")
             {
                 _clientObject.DataType = DataType.BINARY;
-                byte[] data = MessageUtil.BuildReply(_clientObject, 200, "TYPE set to BINARY");
-                _clientObject.Write(data);
-            } else
-            {
-                byte[] data = MessageUtil.BuildReply(_clientObject, 500, "Input TYPE invalid");
-                _clientObject.Write(data);
+                byte[] responseSetBinarydata = MessageUtil.BuildReply(_clientObject, 200, "TYPE set to BINARY");
+                _clientObject.Write(responseSetBinarydata);
+                return;
             }
+
+            byte[] data = MessageUtil.BuildReply(_clientObject, 500, "Input TYPE invalid");
+            _clientObject.Write(data);
         }
 
         public bool ShouldLogin()

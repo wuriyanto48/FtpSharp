9a221d015fe4044735452b1c3169728efc4e296c
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-07-11 20:44:43 +0700

    format code

--- FtpSharp.Server/Src/SessionIdGenerator.cs
+++ FtpSharp.Server/Src/SessionIdGenerator.cs
@@ -10,24 +10,24 @@ namespace FtpSharp.Server
 	
 		public static string RandomString(int size, bool lowerCase = false)  
 		{ 
-		  var builder = new StringBuilder(size);  
+			var builder = new StringBuilder(size);  
 
-		  // Unicode/ASCII Letters are divided into two blocks
-		  // (Letters 65–90 / 97–122):   
-		  // The first group containing the uppercase letters and
-		  // the second group containing the lowercase.  
+			// Unicode/ASCII Letters are divided into two blocks
+			// (Letters 65–90 / 97–122):   
+			// The first group containing the uppercase letters and
+			// the second group containing the lowercase.  
 
-		  // char is a single Unicode character  
-		  char offset = lowerCase ? 'a' : 'A'; 
-		  const int lettersOffset = 26; // A...Z or a..z: length = 26  
+			// char is a single Unicode character  
+			char offset = lowerCase ? 'a' : 'A'; 
+			const int lettersOffset = 26; // A...Z or a..z: length = 26  
 
-		  for (var i = 0; i < size; i++)  
-		  {  
+			for (var i = 0; i < size; i++)  
+			{  
 			var charPos = (char)_random.Next(offset, offset + lettersOffset);  
 			builder.Append(charPos);
-		  }
-          
-		  return lowerCase ? builder.ToString().ToLower() : builder.ToString();  
+			}
+			
+			return lowerCase ? builder.ToString().ToLower() : builder.ToString();  
 		} 
 
         public static string Generate()
